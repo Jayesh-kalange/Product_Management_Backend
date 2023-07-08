a1624979b367d5b0fdcbf744b46e5f44d0e3d017
Jayesh Kalange <jayeshkalange@gmail.com>
2023-07-08 22:47:20 +0530

    First commit

--- src/main/java/com/project/service/ProductService.java
+++ src/main/java/com/project/service/ProductService.java
@@ -1,5 +1,16 @@
 package com.project.service;
 
+import java.util.List;
+
+import com.project.model.Product;
+
 public interface ProductService {
 
+	public Product saveProduct(Product product);
+	
+	public List<Product> getAllProduct();
+	
+	public Product getProductById(Integer id);
+	
+	public void deleteProduct(Integer id);
 }
--- src/main/java/com/project/service/ProductServiceImpl.java
+++ src/main/java/com/project/service/ProductServiceImpl.java
@@ -1,5 +1,36 @@
 package com.project.service;
 
+import java.util.List;
+
+import com.project.model.Product;
+import com.project.repository.ProductRepository;
+
 public class ProductServiceImpl implements ProductService {
+	
+	private ProductRepository productRepo;
+
+	@Override
+	public Product saveProduct(Product product) {
+		
+		return productRepo.save(product);
+	}
+
+	@Override
+	public List<Product> getAllProduct() {
+		
+		return productRepo.findAll();
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		
+		return productRepo.findById(id).get();
+	}
+
+	@Override
+	public void deleteProduct(Integer id) {
+		
+		
+	}
 
 }
